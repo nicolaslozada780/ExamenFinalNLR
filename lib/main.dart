c6bbd4dea773b2061dd2edf4d3376fc06ae2b5b9
nicolas <a2018115260@estudiantes.upsa.edu.bo>
2021-06-19 11:55:12 -0400

    ultimocommit

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 void main()=>runApp(MiApp());
-class MiApp extends StatelessWidget {
+class MiApp extends StatelessWidget 
+{
   const MiApp({Key key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Mi App Final",
-      home: Inicio(),
-    );
+  return MaterialApp(
+  title: "Mi app",
+  home: Inicio(),
+  );
   }
 }
+
 class Inicio extends StatefulWidget {
   Inicio({Key key}) : super(key: key);
 
@@ -21,44 +25,44 @@ class Inicio extends StatefulWidget {
   _InicioState createState() => _InicioState();
 }
 
-class _InicioState extends State<Inicio> {
-  
-  Future <List<Person>>getData() async {
+class _InicioState extends State<Inicio>
+{
+    Future <List<Person>>getData() async {
   var response = await http.get(
     Uri.parse("https://jsonplaceholder.typicode.com/comments"),
     headers: {"Accept":"Application/json"}
   );
-  var data=json.decode(response.body);
+   var data=json.decode(response.body);
   print(data);
-  List<Person> persona=[];
+  List<Person> person=[];
   for(var p in data){
-    Person person=Person(p["Name"], p["Email"]);
-    persona.add(person);
+    Person count=Person(p["id"], p["name"], p["email"]);
+    person.add(count);
   }
-  print(persona.length);
-  return persona;
+  print(person.length);
+  return person;
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-         title:Text("Nico App")
+         title:Text("nlr App")
     ),
+
     body: Container(
       child: FutureBuilder(
         future: getData(),
         builder: (BuildContext  context, AsyncSnapshot snapshot) {
           print(snapshot.data);
           if(snapshot.data == null){
-            return Container(child: Center(child: Text("loading..."),),);
+            return Container(child: Center(child: Text("Loading..."),),);
           }
           else{
             return ListView.builder(
               itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int name){
-                return ListTile(title:Text(snapshot.data[name].email),
-                subtitle: Text(snapshot.data[name].email.toString()),
+              itemBuilder: (BuildContext context, int id){
+                return ListTile(title:Text(snapshot.data[id].name),
+                subtitle: Text(snapshot.data[id].email.toString()),
                 );
 
               },
@@ -70,10 +74,10 @@ class _InicioState extends State<Inicio> {
     );
   }
 }
-
 class Person {
-  final int name;
+  final int id;
+  final String name;
   final String email;
 
-  Person (this.name, this.email);
+  Person (this.id, this.name, this.email);
 }
